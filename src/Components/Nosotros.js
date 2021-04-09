c75a04bd14790e0c6990ef67bf065b52800a9820
Erik Toalá <183404@ids.upchiapas.edu.mx>
2021-04-08 19:36:24 -0500

    Corrección de ortografia

--- src/Components/Nosotros.js
+++ src/Components/Nosotros.js
@@ -8,9 +8,9 @@ const Nosotros = () => {
                 <br></br><br></br><br></br>
                 <h3 className="title">Nosotros</h3>
                 <br></br>
-                <p>Somos una empresa pequeña, con talentos de jovenes en impactar en el mercado a trevés de la tecnología.</p>
+                <p>Somos una empresa pequeña, con talentos de jovenes en impactar en el mercado a través de la tecnología.</p>
                 <p>Aseguramos la continuidad de tu diversión y entretenimiento mediante la creación e innovación del desarrollo de videojuegos.</p>
-                <p>Nos especializamos en brindar servicios de calidad, ofreciendo la felicidad de los usuarios en mano y tranformando la nueva era de la diversión digital. </p>
+                <p>Nos especializamos en brindar servicios de calidad, ofreciendo la felicidad de los usuarios en mano y transformando la nueva era de la diversión digital. </p>
                 <br></br><br></br><br></br><br></br><br></br><br></br>
 
            </div>
